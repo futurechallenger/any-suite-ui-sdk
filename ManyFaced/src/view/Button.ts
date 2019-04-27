f229539a1ec5a7c75e1aab367e68e5c1a7edbeed
Uncle Charlie <lixiangdong@mobike.com>
2019-04-28 00:55:04 +0800

    add jest & config

--- ManyFaced/src/view/Button.ts
+++ ManyFaced/src/view/Button.ts
@@ -1,10 +1,14 @@
 import { CardWidget } from './CardWidget';
+import { CardAction } from '../action/CardAction';
 
 /**
  * Button
  */
 class Button extends CardWidget {
 
+  setOnClickAction(action: CardAction): Button {
+    return this;
+  }
 }
 
 export Button;
